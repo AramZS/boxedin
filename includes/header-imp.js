6871938fe6b16105f06b738bf0bb7cdb93249905
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-07-18 00:13:25 -0400

    Button top justification and color changes

--- includes/header-imp.js
+++ includes/header-imp.js
@@ -18,10 +18,10 @@ jQuery(document).ready(function () {
 	jQuery('.topmostmenu .menu-item').each(function (i) {
 		btncounter++;
 		jQuery(this).addClass("btn " + "btn-" + topmostmenuSize); 
-			if(btncounter = 1) {
+			if(btncounter == 1) {
 				jQuery(this).addClass("btn-" + topmostmenuOne); 
 			}
-			if(btncounter = 2) {
+			if(btncounter == 2) {
 				jQuery(this).addClass("btn-" + topmostmenuTwo);  
 			}
 			if(btncounter > 2) {
@@ -33,6 +33,22 @@ jQuery(document).ready(function () {
 	
 		var addedlogowidth = logowidth + 10;
 		jQuery('.top-head-menu').css("margin-left",addedlogowidth);
+		
+		
+    var children = $('.topmostmenu .menu-item');
+	var itemSpace = 0;
+	var itemHeight = 0;
+	children.each(function(item){
+		itemSpace += $(this).width();
+		itemHeight = $(this).height();
+	});
+ 	var margin = Math.floor(($('.topmostmenu').width() - itemSpace) / (children.size() - 1)) - 2;
+ 	children.each(function(item){
+ 		margin = margin - addedlogowidth;
+   		if(item != 0) {
+			$(this).css('margin-left', margin + 'px' );
+   		}
+	});
 	
 
 });
